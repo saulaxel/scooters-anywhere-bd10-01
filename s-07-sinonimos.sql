6cb6f9f562c3407579af66745be4043594260e1e
EnigmaK9 <enigmak9@protonmail.com>
2022-01-03 20:06:52 -0600

    added 's-07-sinonimos.sql' file

---
+++ s-07-sinonimos.sql
@@ -0,0 +1,44 @@
+--@Autor:           Martínez Ortiz Saúl Axel, Padilla Herrera Carlos Ignacio
+--@Fecha creación:  2022/01/03
+--@Descripción:     Creación de sinónimos
+
+prompt Connect as admin to create public synonyms
+connect mp_proy_admin/mp
+
+prompt creando o reemplazando sinónimos públicos con PL/SQL
+
+set serveroutput on
+declare
+  v_create varchar2(1000);
+  v_synonym varchar2(1000);
+  v_grant varchar2(1000);
+  cursor cur_entidades is
+  select table_name
+  from all_tables
+  where owner = 'MP_PROY_ADMIN';
+begin
+ for p in cur_entidades loop
+   v_synonym := 'MP_' || p.table_name;
+   v_grant := 'grant select on '
+   || 'mp_proy_admin.'
+   || p.table_name
+   || ' to '
+   || 'mp_proy_invitado'
+   ;
+   v_create := 'create or replace public synonym '
+   || v_synonym
+   || ' for ' 
+   ||'mp_proy_admin.'
+   || p.table_name
+   ;
+   execute immediate v_grant;
+   execute immediate v_create;
+   end loop;
+ end;
+ 
+ /
+ 
+-- execute immediate 
+--select * from mp_servicio;
+-- grant select on mp_proy_admin.SERVICIOS to mp_proy_invitado
+--create or replace public synonym MP_SCOOTER for mp_proy_admin.SCOOTER;
